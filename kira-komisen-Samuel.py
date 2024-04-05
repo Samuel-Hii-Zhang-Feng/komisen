edd88780e949acd7816bf5d987f5b3fe0b9b0a9b
Samuel-Hii-Zhang-Feng <163367726+Samuel-Hii-Zhang-Feng@users.noreply.github.com>
2024-04-04 17:36:22 -0700

    Update kira-komisen-Samuel.py

--- kira-komisen-Samuel.py
+++ kira-komisen-Samuel.py
@@ -1,7 +1,10 @@
+#Atur Cara Mengira Komisen
+#Pengisytiharan 
 jumlah = 0
 jualan = 0
 komisen = 0
 ulang = "Y"
+#Proses
 while ulang == "Y":
     jualan = float(input("Masukkan jumlah jualan:RM"))
     if jualan > 80:
@@ -15,9 +18,11 @@ while ulang == "Y":
     else:
         kadar_komisen = 0.02
     komisen = jualan * kadar_komisen
+    #Output
     print("Komisen anda ialah RM",round(komisen,2))
     jumlah = jumlah + komisen
+    #Input
     ulang = input("Masukkan Y untuk terus atau N untuk berhenti")
-        
+#Output  
 print("\nJumlah komisen ialah RM", round(jumlah,2))
 print("Sekian,tamat")
